9e3b3e1986ac93acccf6cb1e06e7c44febca50cf
Bryan Romas <bcromas@gmail.com>
2018-11-13 15:44:33 -0500

    instantiating ZillowHomes based on results

--- 507_city_prospects.py
+++ 507_city_prospects.py
@@ -15,6 +15,23 @@ except:
     CACHE_DICT = {}
 #end caching functionality
 
+#object for capturing Zillow home details
+class ZillowHome():
+    def __init__(self, streetAddress, beds=None, baths=None, rooms=None, sqft=None, price=None, price_sqft=None, est_mortgage=None, url=None):
+        self.streetAddress = streetAddress
+        self.beds = beds
+        self.baths = baths
+        self.rooms = rooms
+        self.sqft = sqft
+        self.price = price
+        self.price_sqft = price_sqft
+        self.est_mortgage = est_mortgage
+        self.url = url
+
+    def __str__(self):
+        return "{} - Price:{} - Price/SQFT:{}".format(self.streetAddress, self.price,self.price_sqft)
+
+
 #start of simple check of cache
 def check_cache(url):
 
@@ -32,26 +49,31 @@ def check_cache(url):
         return CACHE_DICT[url]
 #end of simple check of cache
 
+#start of crawling Zillow for homes
 def home_prices(city):
     zil_base_url = 'https://www.zillow.com/homes/for_sale'
     zil_city_url = zil_base_url+"/"+city
     city_in_cache = check_cache(zil_city_url)
     city_soup = BeautifulSoup(city_in_cache,'html.parser')
     photo_cards = city_soup.find(class_="photo-cards")
+    # test_list = []
     for i in photo_cards:
         try:
             home_url = i.a['href']
             zil_home_url = zil_base_url+home_url
             home_in_cache = check_cache(zil_home_url)
+            home_soup = BeautifulSoup(home_in_cache,'html.parser')
+            zil_streetAddress = home_soup.find(class_="zsg-h1 hdp-home-header-st-addr").text
+            x = ZillowHome(streetAddress=zil_streetAddress)
+            test_list.append(x)
+            #call ZillowHome with streetAddress, beds, baths, rooms, sqft, price, price_sqft, est_mortgage, url=None)
+
         except:
             pass
 
-    #the lines below will find the summary table at the bottom of each page, need to
-    # facts_table = url_soup.find(class_="zsg-table")
-    # table_rows = facts_table.find_all('tr')
-    # for i in table_rows:
-    #     print(i.text)
-    #     print("*"*20)
+    for i in test_list: #continue adding attributes from home page to call to ZillowHome(), uncomment line 59, 
+        print(i)
+#end of crawling Zillow for homes
 
 if __name__ == "__main__":
     while True:
